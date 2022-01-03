93eecb0c5aa1c49906cb8c027eb0d41327f6ae36
shadman17 <shadman.wasif@gmail.com>
2022-01-03 18:02:42 +0600

    Models Created

--- App_login/apps.py
+++ App_login/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AppLoginConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'App_login'
+    name = 'App_Login'
--- sample_blog_project/settings.py
+++ sample_blog_project/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'App_login',
+    'App_Login',
     'App_Blog',
 ]
 
